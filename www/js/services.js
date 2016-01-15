0143b062bda00095f660370eb56d873e66c3f8c7
Kevin Vandersmissen (imec) <kvdsmis@imec.be>
2016-01-15 18:53:50 +0100

    First commit

---
+++ www/js/services.js
@@ -0,0 +1,11 @@
+fbook.factory('recipeService',function($firebaseArray){
+	var fb = new Firebase("https://flickering-inferno-2980.firebaseio.com/");
+	var recs = $firebaseArray(fb);
+	var recipeService = {
+		all: recs, 
+		get: function(recId) {
+			return recs.$getRecord(recId);
+		}
+	};
+	return recipeService;
+});
